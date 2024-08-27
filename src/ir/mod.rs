4c7760082959de0680fc9b1bf668f74352df386b
Rik Huijzer <github@huijzer.xyz>
2024-08-27 11:38:37 +0200

    Update

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,3 +1,12 @@
 pub mod block;
 pub mod operation;
 pub mod region;
+
+/// This is the trait that is implemented by all operations.
+/// FuncOp, for example, will be implemented by various dialects.
+/// Note that the parser will parse the tokens into an `Operation`
+/// and MLIR would cast the `Operation` into a specific `Op` variant
+/// such as `FuncOp`.
+pub trait Op {
+    fn name(&self) -> &'static str;
+}
