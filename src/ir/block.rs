edeb857055d67985ee99ae04b9684d565b6d5b65
Rik Huijzer <github@huijzer.xyz>
2024-08-27 11:59:49 +0200

    Update

--- src/ir/block.rs
+++ src/ir/block.rs
@@ -1,9 +1,10 @@
 use crate::ir::operation::Operation;
+
 struct BlockArgument {
     name: String,
 }
 
 pub struct Block {
     arguments: Vec<BlockArgument>,
-    operations: Vec<Box<dyn Operation>>,
+    // operations: Vec<Box<dyn Operation>>,
 }
--- src/ir/operation.rs
+++ src/ir/operation.rs
@@ -1,3 +1,8 @@
+use crate::Attributes;
+
+struct OperationName {
+    name: String, // TODO: Should be StringAttr,
+}
 
 // Takes attributes
 //
@@ -8,13 +13,29 @@
 // `ConstantOp` does also not specify fields since they are accessed
 // via a pointer to the `Operation`.
 // In MLIR, a specific Op can be casted from an Operation.
-pub trait Operation {
-    fn parse(input: &str) -> Option<Self>
-    where
-        Self: Sized;
-
-    fn name(&self) -> &'static str;
+// The operation also represents functions and modules.
+pub struct Operation {
+    name: OperationName,
+    operands: i64,
+    attributes: Attributes,
+}
+impl Operation {
+    pub fn parse(input: &str) -> Option<Self> {
+        todo!()
+    }
+    fn name(&self) -> &'static str {
+        todo!()
+    }
     fn print(&self) -> String {
         todo!() 
     }
+}
+
+/// This is the trait that is implemented by all operations.
+/// FuncOp, for example, will be implemented by various dialects.
+/// Note that the parser will parse the tokens into an `Operation`
+/// and MLIR would cast the `Operation` into a specific `Op` variant
+/// such as `FuncOp`.
+pub trait Op {
+    fn name(&self) -> &'static str;
 }
