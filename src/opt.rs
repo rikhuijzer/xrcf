21eee05e1f265126bcbf903679532fa3a712a096
Rik Huijzer <github@huijzer.xyz>
2024-09-25 08:44:36 +0200

    Cleanup

--- src/opt.rs
+++ src/opt.rs
@@ -2,26 +2,25 @@ use crate::parser::BuiltinParse;
 use crate::parser::Parser;
 use core::fmt::Error;
 use core::fmt::Write;
+use crate::ir::Op;
 
 pub trait Transform {
-    /// Transform the source code to the target code.
-    // TODO: This should take a memory object not a string.
-    fn transform(&self, src: &str, out: &mut dyn Write) -> Result<(), Error>;
+    fn transform(&self, from: &dyn Op, to: &mut dyn Write) -> Result<(), Error>;
 }
 
 struct MLIRToLLVMIRTranslation {}
 
 impl Transform for MLIRToLLVMIRTranslation {
-    fn transform(&self, _src: &str, _out: &mut dyn Write) -> Result<(), Error> {
+    fn transform(&self, _from: &dyn Op, _to: &mut dyn Write) -> Result<(), Error> {
         todo!()
     }
 }
 
-pub struct Options {
+pub struct OptOptions {
     canonicalize: bool,
 }
 
-pub fn opt(src: &str, options: Options) -> String {
+pub fn opt(src: &str, options: OptOptions) -> String {
     let module = Parser::<BuiltinParse>::parse(src).unwrap();
     if options.canonicalize {
         println!("canonicalize");
