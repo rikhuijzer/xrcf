98baebad08c371d68d32ff64045cacf65865be09
Rik Huijzer <github@huijzer.xyz>
2024-10-01 10:37:56 +0200

    Fix stackoverflow

--- src/ir/value.rs
+++ src/ir/value.rs
@@ -102,6 +102,6 @@ impl OpOperand {
 
 impl Display for OpOperand {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self)
+        write!(f, "{}", self.operand_name)
     }
 }
