30695d9c7340e8b5b04bbbc360cce02d0229e688
Rik Huijzer <github@huijzer.xyz>
2024-10-08 16:00:55 +0200

    Add `DeadCodeElimination` pass

--- src/canonicalize.rs
+++ src/canonicalize.rs
@@ -7,35 +7,58 @@ pub enum CanonicalizeResult {
     Unchanged,
 }
 
-pub fn canonicalize_op(op: &dyn Op) -> CanonicalizeResult {
-    let region = op.region();
-    if let Some(region) = region {
-        let mut changed = CanonicalizeResult::Unchanged;
-        for block in region.read().unwrap().blocks() {
-            let block = block.read().unwrap();
-            let ops = block.ops();
-            let ops = ops.read().unwrap();
-            let readonly_copy = ops.clone();
-            drop(ops);
-            for op in readonly_copy.iter() {
-                let op: &dyn Op = &*op.read().unwrap();
-                let result = canonicalize_op(op);
-                if result == CanonicalizeResult::Changed {
-                    changed = CanonicalizeResult::Changed;
+pub trait Canonicalize {
+    fn canonicalize(&self, op: &dyn Op) -> CanonicalizeResult;
+}
+
+struct CanonicalizeOp;
+
+impl Canonicalize for CanonicalizeOp {
+    fn canonicalize(&self, op: &dyn Op) -> CanonicalizeResult {
+        let region = op.region();
+        if let Some(region) = region {
+            let mut changed = CanonicalizeResult::Unchanged;
+            for block in region.read().unwrap().blocks() {
+                let block = block.read().unwrap();
+                let ops = block.ops();
+                let ops = ops.read().unwrap();
+                let readonly_copy = ops.clone();
+                drop(ops);
+                for op in readonly_copy.iter() {
+                    let op: &dyn Op = &*op.read().unwrap();
+                    let result = CanonicalizeOp.canonicalize(op);
+                    if result == CanonicalizeResult::Changed {
+                        changed = result;
+                    }
                 }
             }
+            changed
+        } else {
+            op.canonicalize()
         }
-        changed
-    } else {
-        op.canonicalize()
+    }
+}
+
+struct DeadCodeElimination;
+
+impl Canonicalize for DeadCodeElimination {
+    fn canonicalize(&self, op: &dyn Op) -> CanonicalizeResult {
+        CanonicalizeResult::Unchanged
     }
 }
 
 pub fn canonicalize(module: &mut ModuleOp) {
     let max_iterations = 16;
     for _ in 0..max_iterations {
-        let result = canonicalize_op(module);
-        if result == CanonicalizeResult::Unchanged {
+        let canonicalizers: Vec<&dyn Canonicalize> = vec![&CanonicalizeOp, &DeadCodeElimination];
+        let mut changed = false;
+        for canonicalizer in canonicalizers {
+            let result = canonicalizer.canonicalize(module);
+            if result == CanonicalizeResult::Changed {
+                changed = true;
+            }
+        }
+        if !changed {
             break;
         }
     }
