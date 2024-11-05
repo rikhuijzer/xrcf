2bd2b7e6003826abe5d321dac419caa7cd8aba83
Rik Huijzer <github@huijzer.xyz>
2024-11-05 19:35:38 +0100

    Can add matrices

--- xrcf/tests/calculate.py
+++ xrcf/tests/calculate.py
@@ -1,12 +1,26 @@
-from ctypes import CDLL, c_int
+from ctypes import CDLL, c_int, Structure, c_int16, POINTER, byref
 import os
 import sys
 
+class Vec16x16(Structure):
+    _fields_ = [("values", c_int16 * 16)]
+
+    def __repr__(self):
+        return f"Vec16x16({list(self.values)})"
+
 ll = """
 define i32 @calculate(i32 %a, i32 %b) #0 {
-    %1 = mul i32 %a, %b
-    %2 = add i32 %1, 43
-    ret i32 %2
+  %1 = mul i32 %a, %b
+  %2 = add i32 %1, 43
+  ret i32 %2
+}
+
+define void @calc_vec(<16 x i16>* %result, <16 x i16>* %a, <16 x i16>* %b) #0 {
+  %1 = load <16 x i16>, <16 x i16>* %a, align 32
+  %2 = load <16 x i16>, <16 x i16>* %b, align 32
+  %3 = add <16 x i16> %1, %2
+  store <16 x i16> %3, <16 x i16>* %result, align 32
+  ret void
 }
 
 attributes #0 = { nounwind }
@@ -20,9 +34,23 @@ def load_lib():
     lib = CDLL("./tmp.so")
     lib.calculate.argtypes = [c_int, c_int]
     lib.calculate.restype = c_int
+    lib.calc_vec.argtypes = [POINTER(Vec16x16), POINTER(Vec16x16), POINTER(Vec16x16)]
+    lib.calc_vec.restype = None
     return lib
 
 
 lib = load_lib()
-result = lib.calculate(5, 3)
-print(f"Result: {result}")
+result = lib.calculate(1, 2)
+print(f"calculate: {result}")
+
+a = Vec16x16()
+b = Vec16x16()
+for i in range(1, 8):
+    a.values[i] = 1
+    b.values[i] = 2
+for i in range(8, 16):
+    a.values[i] = 3
+    b.values[i] = 4
+result = Vec16x16()
+lib.calc_vec(byref(result), a, b)
+print(f"calc_vec: {result}")
