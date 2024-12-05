0a23c813ad6a59ae4927e48795253f749f93809b
Rik Huijzer <github@huijzer.xyz>
2024-12-05 14:51:06 +0100

    Support `scf.if` without `yield` (#28)
    Allows lowering
    ```mlir
    func.func @main() -> i64 {
      %x = arith.constant 0 : i1
      scf.if %x {
        %0 = arith.constant 0 : i64
      } else {
        %1 = arith.constant 1 : i64
      }
      %2 = arith.constant 0 : i64
      return %2 : i64
    }
    ```
    to
    ```mlir
    func.func @main() -> i64 {
      %x = arith.constant false
      cf.cond_br %x, ^bb1, ^bb2
    ^bb1: 
      %0 = arith.constant 0 : i64
      cf.br ^bb3
    ^bb2:
      %1 = arith.constant 1 : i64
      cf.br ^bb3
    ^bb3:
      %2 = arith.constant 0 : i64
      return %2 : i64
    }
    ```
    Similar to `mlir-opt --convert-scf-to-cf`.

--- xrcf/tests/convert/scf_to_cf.rs
+++ xrcf/tests/convert/scf_to_cf.rs
@@ -51,3 +51,41 @@ fn test_if() {
     Tester::verify(module);
     Tester::check_lines_exact(&actual, expected, Location::caller());
 }
+
+#[test]
+fn test_if_without_yield() {
+    Tester::init_tracing();
+    let src = indoc! {r#"
+    func.func @main() -> i64 {
+      %x = arith.constant false
+      scf.if %x {
+        %0 = arith.constant 0 : i64
+      } else {
+        %1 = arith.constant 1 : i64
+      }
+      %2 = arith.constant 0 : i64
+      return %2 : i64
+    }
+    "#};
+    let expected = indoc! {r#"
+    func.func @main() -> i64 {
+      %x = arith.constant false
+      cf.cond_br %x, ^bb1, ^bb2
+    ^bb1: 
+      %0 = arith.constant 0 : i64
+      cf.br ^bb3
+    ^bb2:
+      %1 = arith.constant 1 : i64
+      cf.br ^bb3
+    ^bb3:
+      %2 = arith.constant 0 : i64
+      return %2 : i64
+    }
+    "#};
+    let (module, actual) = Tester::parse(src);
+    Tester::verify(module);
+    Tester::check_lines_contain(&actual, &src, Location::caller());
+    let (module, actual) = Tester::transform(flags(), src);
+    Tester::verify(module);
+    Tester::check_lines_contain(&actual, expected, Location::caller());
+}
