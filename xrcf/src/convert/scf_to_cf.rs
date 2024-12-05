5032a9bbb037af9a2757e7f3117cabb68512c8aa
Rik Huijzer <github@huijzer.xyz>
2024-12-05 12:35:22 +0100

    Update

--- xrcf/src/convert/scf_to_cf.rs
+++ xrcf/src/convert/scf_to_cf.rs
@@ -39,15 +39,16 @@ fn add_block_from_region(
 ) -> Result<Arc<RwLock<OpOperand>>> {
     let mut ops = region.ops();
     let ops_clone = ops.clone();
-    let yield_op = ops_clone.last().unwrap();
-    let yield_op = yield_op.try_read().unwrap();
-    let yield_op = yield_op
+    let last_op = ops_clone.last().unwrap();
+    let last_op = last_op.try_read().unwrap();
+    let yield_op = last_op
         .as_any()
-        .downcast_ref::<dialect::scf::YieldOp>()
-        .unwrap();
-    let new_op = lower_yield_op(&yield_op, merge_label)?;
-    ops.pop();
-    ops.push(new_op.clone());
+        .downcast_ref::<dialect::scf::YieldOp>();
+    if let Some(yield_op) = yield_op {
+        let new_op = lower_yield_op(&yield_op, merge_label)?;
+        ops.pop();
+        ops.push(new_op.clone());
+    }
 
     let unset_block = parent_region.add_empty_block();
     let block = unset_block.set_parent(Some(parent_region.clone()));
