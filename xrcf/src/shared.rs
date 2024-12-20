f1d603d4ba69e27782aaabd062a7187aecefefd8
Rik Huijzer <github@huijzer.xyz>
2024-12-20 10:42:23 +0100

    Update

--- xrcf/src/shared.rs
+++ xrcf/src/shared.rs
@@ -3,75 +3,95 @@ use std::sync::RwLock;
 use std::sync::RwLockReadGuard;
 use std::sync::RwLockWriteGuard;
 
-struct SharedGuard<T> {
-    inner: Arc<RwLock<T>>,
-}
-
-impl<T> SharedGuard<T> {
-    fn new(inner: T) -> Self {
-        Self {
-            inner: Arc::new(RwLock::new(inner)),
-        }
-    }
-    fn read(&self) -> RwLockReadGuard<T> {
-        self.inner.try_read().unwrap()
-    }
-    fn write(&self) -> RwLockWriteGuard<T> {
-        self.inner.try_write().unwrap()
-    }
-    fn try_read(&self) -> RwLockReadGuard<T> {
-        self.inner.try_read().unwrap()
-    }
-    fn try_write(&self) -> RwLockWriteGuard<T> {
-        self.inner.try_write().unwrap()
-    }
-}
-
-/// A shared object that can be read and written to.
-///
-/// Compared to `Arc<RwLock<T>>`, this API slightly less verbose.
+/// A convenience type alias for [Arc<RwLock<T>>].
 ///
 /// # Example
 ///
 /// ```
-/// use xrcf::ir::Region;
+/// use std::sync::Arc;
+/// use std::sync::RwLock;
 /// use xrcf::shared::Shared;
 ///
-/// let region = Region::default();
-/// let _ = region.add_empty_block();
-/// let shared = Shared::new(region);
-/// assert_eq!(shared.read().blocks().into_iter().len(), 1);
+/// let lock = Shared::new(42.into());
+/// assert_eq!(*lock.try_read().unwrap(), 42);
 /// ```
-/// And the same now using `Arc<RwLock<T>>`:
+/// can also be used together with [SharedExt]:
 /// ```
-/// use std::sync::{Arc, RwLock};
-/// use xrcf::ir::Region;
+/// use std::sync::Arc;
+/// use std::sync::RwLock;
+/// use xrcf::shared::Shared;
+/// use xrcf::shared::SharedExt;
 ///
-/// let region = Region::default();
-/// let _unset_block = region.add_empty_block();
-/// let shared = Arc::new(RwLock::new(region));
-/// assert_eq!(shared.try_read().unwrap().blocks().into_iter().len(), 1);
+/// let lock = Shared::new(42.into());
+/// assert_eq!(*lock.rd(), 42);
 /// ```
-pub struct Shared<T> {
-    inner: SharedGuard<T>,
+pub type Shared<T> = Arc<RwLock<T>>;
+
+/// A trait for creating new Arc<RwLock<T>> values
+pub trait SharedExt<T> {
+    fn new(value: T) -> Self;
 }
 
-impl<T> Shared<T> {
-    pub fn new(inner: T) -> Self {
-        Self {
-            inner: SharedGuard::new(inner),
-        }
+impl<T> SharedExt<T> for Shared<T> {
+    fn new(value: T) -> Self {
+        Arc::new(RwLock::new(value))
     }
-    pub fn read(&self) -> RwLockReadGuard<T> {
-        self.inner.read()
+}
+
+#[test]
+fn test_shared() {
+    let lock = Shared::new(42.into());
+    assert_eq!(*lock.try_read().unwrap(), 42);
+}
+
+/// A convenience trait for the `RwLock` API.
+///
+/// This trait makes using [RwLock] less verbose. Is this trait pretty? No. But
+/// in the end it's just a convenience trait that could easily be removed later.
+/// It does add some cognitive load for the read, but compared to
+/// `try_read().unwrap()` it saves a **lot** of typing.
+///
+/// # Example
+///
+/// With this trait:
+/// ```
+/// use std::sync::Arc;
+/// use std::sync::RwLock;
+/// use xrcf::shared::RwLockExt;
+///
+/// let lock = Arc::new(RwLock::new(42));
+/// assert_eq!(*lock.rd(), 42);
+/// ```
+/// and with `Shared`:
+/// ```
+/// use std::sync::Arc;
+/// use std::sync::RwLock;
+/// use xrcf::shared::RwLockExt;
+/// use xrcf::shared::Shared;
+///
+/// let lock = Shared::new(42.into());
+/// assert_eq!(*lock.rd(), 42);
+/// ```
+/// Without this trait:
+/// ```
+/// use std::sync::Arc;
+/// use std::sync::RwLock;
+///
+/// let lock = Arc::new(RwLock::new(42));
+/// assert_eq!(*lock.try_read().unwrap(), 42);
+/// ```
+pub trait RwLockExt<T> {
+    /// Convenience method for reading.
+    fn rd(&self) -> RwLockReadGuard<T>;
+    /// Convenience method for writing.
+    fn wr(&self) -> RwLockWriteGuard<T>;
+}
+
+impl<T> RwLockExt<T> for Arc<RwLock<T>> {
+    fn rd(&self) -> RwLockReadGuard<T> {
+        self.try_read().unwrap()
     }
-    pub fn write(&self) -> RwLockWriteGuard<T> {
-        self.inner.write()
-    }
-    pub fn try_read(&self) -> RwLockReadGuard<T> {
-        self.inner.try_read()
-    }
-    pub fn try_write(&self) -> RwLockWriteGuard<T> {
-        self.inner.try_write()
+    fn wr(&self) -> RwLockWriteGuard<T> {
+        self.try_write().unwrap()
     }
 }
