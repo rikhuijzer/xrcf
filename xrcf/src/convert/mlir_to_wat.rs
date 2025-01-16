391df7ebcd78917fbb5c7355c67ac0dedea7c6b8
Rik Huijzer <github@huijzer.xyz>
2025-01-16 21:20:01 +0100

    Update

--- xrcf/src/convert/mlir_to_wat.rs
+++ xrcf/src/convert/mlir_to_wat.rs
@@ -1,4 +1,5 @@
 use crate::convert::apply_rewrites;
+use crate::convert::simple_op_rewrite;
 use crate::convert::ChangedOp;
 use crate::convert::Pass;
 use crate::convert::Rewrite;
@@ -24,16 +25,7 @@ impl Rewrite for AddiLowering {
         true
     }
     fn rewrite(&self, op: Shared<dyn Op>) -> Result<RewriteResult> {
-        let op = op.rd();
-        let op = match op.as_any().downcast_ref::<arith::AddiOp>() {
-            Some(op) => op,
-            None => return Ok(RewriteResult::Unchanged),
-        };
-        let operation = op.operation().clone();
-        let new_op = wat::AddOp::from_operation_arc(operation);
-        let new_op = Shared::new(new_op.into());
-        op.replace(new_op.clone());
-        Ok(RewriteResult::Changed(ChangedOp::new(new_op)))
+        simple_op_rewrite::<arith::AddiOp, wat::AddOp>(op)
     }
 }
 
@@ -66,6 +58,20 @@ impl Rewrite for FuncLowering {
     }
 }
 
+struct SubiLowering;
+
+impl Rewrite for SubiLowering {
+    fn name(&self) -> &'static str {
+        "convert_mlir_to_wat::SubiLowering"
+    }
+    fn parallelizable(&self) -> bool {
+        true
+    }
+    fn rewrite(&self, op: Shared<dyn Op>) -> Result<RewriteResult> {
+        simple_op_rewrite::<arith::SubiOp, wat::SubOp>(op)
+    }
+}
+
 struct ModuleLowering;
 
 impl Rewrite for ModuleLowering {
@@ -76,16 +82,7 @@ impl Rewrite for ModuleLowering {
         true
     }
     fn rewrite(&self, op: Shared<dyn Op>) -> Result<RewriteResult> {
-        let op = op.rd();
-        let op = match op.as_any().downcast_ref::<ir::ModuleOp>() {
-            Some(op) => op,
-            None => return Ok(RewriteResult::Unchanged),
-        };
-        let operation = op.operation().clone();
-        let new_op = wat::ModuleOp::from_operation_arc(operation);
-        let new_op = Shared::new(new_op.into());
-        op.replace(new_op.clone());
-        Ok(RewriteResult::Changed(ChangedOp::new(new_op)))
+        simple_op_rewrite::<ir::ModuleOp, wat::ModuleOp>(op)
     }
 }
 
@@ -99,16 +96,7 @@ impl Rewrite for ReturnLowering {
         true
     }
     fn rewrite(&self, op: Shared<dyn Op>) -> Result<RewriteResult> {
-        let op = op.rd();
-        let op = match op.as_any().downcast_ref::<func::ReturnOp>() {
-            Some(op) => op,
-            None => return Ok(RewriteResult::Unchanged),
-        };
-        let operation = op.operation().clone();
-        let new_op = wat::ReturnOp::from_operation_arc(operation);
-        let new_op = Shared::new(new_op.into());
-        op.replace(new_op.clone());
-        Ok(RewriteResult::Changed(ChangedOp::new(new_op)))
+        simple_op_rewrite::<func::ReturnOp, wat::ReturnOp>(op)
     }
 }
 
@@ -121,6 +109,7 @@ impl Pass for ConvertMLIRToWat {
         let rewrites: Vec<&dyn Rewrite> = vec![
             &AddiLowering,
             &FuncLowering,
+            &SubiLowering,
             &ModuleLowering,
             &ReturnLowering,
         ];
